a200e4421310efad44f42bb10cc492759f393555
Marc <ludovicimarc@gmail.com>
2021-10-08 10:21:26 +0200

    added check for browser env

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import GlobalStyles from './styles'
 import Pages from './pages'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
+// eslint-disable-next-line no-new-func
+var isBrowser = new Function(
+	'try {return this===window;}catch(e){ return false;}'
+)
+
 const client = new ApolloClient({
-	uri: process.env.production
+	uri: isBrowser()
 		? 'https://ml-apollo-server-catstronauts.herokuapp.com/'
 		: 'http://localhost:4000',
 	cache: new InMemoryCache(),

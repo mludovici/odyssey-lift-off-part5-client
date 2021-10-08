1c5ae3b862bd57bc22310ed6df37eda8bb2bda2a
Marc <ludovicimarc@gmail.com>
2021-10-08 12:36:41 +0200

    deleted deprecated field length

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ import GlobalStyles from './styles'
 import Pages from './pages'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
-// eslint-disable-next-line no-new-func
-var isBrowser = new Function(
-	'try {return this===window;}catch(e){ return false;}'
-)
-
 const client = new ApolloClient({
-	uri: isBrowser()
-		? 'https://ml-apollo-server-catstronauts.herokuapp.com/'
-		: 'http://localhost:4000',
+	uri: 'https://ml-apollo-server-catstronauts.herokuapp.com/',
 	cache: new InMemoryCache(),
 })
 

32e435d046fafcd4c19c6d3d6218a29a76341b95
Marc <ludovicimarc@gmail.com>
2021-10-08 10:07:30 +0200

    added heroku url

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyles from './styles'
+import Pages from './pages'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
-});
+	uri: process.env.production
+		? 'https://ml-apollo-server-catstronauts.herokuapp.com/'
+		: 'http://localhost:4000',
+	cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
+	<ApolloProvider client={client}>
+		<GlobalStyles />
+		<Pages />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
